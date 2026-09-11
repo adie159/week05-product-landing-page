929eac66dd390e7ac37dadf413419e9d99f2eab2
adie159 <frandiekalvin@gmail.com>
2026-09-11 08:33:14 +0800

    feat: create landing page layout

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -150,7 +150,7 @@
             </x-testimonial-card>
 
             <x-testimonial-card name="Julius P." role="Member since 2023" initial="J">
-                Nag-start ako sa daily pass, tapos nag-monthly na ako dahil ang saya ng environment dito.
+                Nagstart ako sa daily pass, tapos nag-monthly na ako dahil ang saya ng environment dito.
             </x-testimonial-card>
         </div>
     </section>
